3f0ce9850f179ec4794faf964d877a4c8399d4d1
Dana Le <mydoan0411@gmail.com>
2025-11-11 01:32:56 -0500

    Add files via upload

---
+++ JS-LAB-8-DateMathTimers-25f/lab8-1-countdown.js
@@ -0,0 +1,39 @@
+﻿/* LAB 8-1 - FINAL COUNTDOWN!! */
+
+
+//create page load listener
+window.addEventListener('load', pageLoad);
+//create page load function
+function pageLoad(){
+	//create variables for required HTML elements
+var todayElement = document.getElementById('todayData');
+    var dueDateElement = document.getElementById('finalData');
+    var messageElement = document.getElementById('dueData');
+    var countMsgElement = document.getElementById('countMsg');
+
+	//create variables for now date and due date
+	var nowDate = new Date();
+console.log(nowDate);
+
+    var dueDate = new Date('December 12, 2024 18:00:00');
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	todayElement.innerHTML= nowDate.toDateString();
+dueDateElement.innerHTML= dueDate.toDateString();
+
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+var nowUTC = nowDate.getTime();
+var dueDateUTC = dueDate.getTime();
+var timeDiff = dueDateUTC - nowUTC;
+	console.log(timeDiff);
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+var numDays = Math.floor(timeDiff/ (1000*60*60*24));
+console.log(numDays);
+				//1000 = milliseconds in 1 second, 60 secs in 1 min, 60 mins in 1 hour, 24 hours in a day. So we're calculating the milliseconds in a day. timeDiff divided this to get the days til dueDate.
+
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+if (numDays <= 0) {
+        countMsgElement.innerHTML = 'The Deadline for the Final Assignment has passed!';
+    } else {
+        messageElement.innerHTML = numDays;
+    }
+}
---
+++ JS-LAB-8-DateMathTimers-25f/lab8-2-clock.js
@@ -0,0 +1,54 @@
+﻿/* LAB 8.2 - STOP TIME */
+
+
+//create page load listener
+window.addEventListener('load', pageLoad);
+
+//create page load function
+function pageLoad() {
+
+	//create variables for required HTML elements
+	var hours = document.getElementById('hoursOut');
+	var mins = document.getElementById('minsOut');
+	var secs = document.getElementById('secsOut');
+	//buttons
+	var btn1 = document.getElementById('btnStart');
+	var btn2 = document.getElementById('btnStop');
+
+	//create time variable so all functions have access to it
+	var clockA;
+
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	function timeDisplay() {
+		var nowDate = new Date();
+		var currentHrs = nowDate.getHours();
+		var currentMins = nowDate.getMinutes();
+		var currentSecs = nowDate.getSeconds();
+
+		hours.innerHTML = currentHrs;
+		mins.innerHTML = ':' + currentMins;
+		secs.innerHTML = ':' + currentSecs;
+	}
+
+//add zero
+
+
+	//CREATE FUNCTION TO START THE CLOCK.
+	function startClock() {
+		clockA = setInterval(timeDisplay, 1000);
+
+	}
+
+	//CREATE FUNCTION TO STOP THE CLOCK
+	function stopClock() {
+		timeDisplay();
+		clearInterval(clockA);
+	}
+
+	// SET EVENT LISTENERS
+	//document.getElementById('btnStart').addEventListener('click',startClock);
+	//document.getElementById('btnStop').addEventListener('click',stopClock);
+	btn1.onclick = startClock;
+	btn2.onclick = stopClock;
+
+}
